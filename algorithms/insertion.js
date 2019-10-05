df616ed082c4a8d369cc56ab03208cc02fab0f4d
unknown <ebbengabogen@gmail.com>
2019-10-05 11:46:31 +0800

    edited insertion sort

--- algorithms/insertion.js
+++ algorithms/insertion.js
@@ -1,8 +1,21 @@
+const arg = require("../plugins/argumentFilter");
+
+let order = "asc";
+let by;
+
 const insertionsort = (arr, num) => {
   let hole = num;
   let value = arr[num];
   for (let i = num; i > 0; i--) {
-    if (value < arr[i - 1]) {
+    if (
+      by === undefined
+        ? order === "desc"
+          ? value > arr[i - 1]
+          : value < arr[i - 1]
+        : order === "desc"
+        ? value[by] > arr[i - 1][by]
+        : value[by] < arr[i - 1][by]
+    ) {
       arr[hole] = arr[i - 1];
       hole = i - 1;
     } else {
@@ -12,14 +25,16 @@ const insertionsort = (arr, num) => {
   arr[hole] = value;
   if (arr.length - 1 > num) {
     insertionsort(arr, num + 1);
-  } else {
-    return arr;
   }
+
+  return arr;
 };
 
-const insertion = arr => {
-  insertionsort(arr, 1);
-  return arr;
+const insertion = (arr, orderArg = [], byArg) => {
+  let params = arg(arr, orderArg, byArg);
+  order = params.order;
+  by = params.by;
+  return insertionsort(arr, 1);
 };
 
 module.exports = insertion;
