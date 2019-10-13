57ed1744ea3546d2a90702340c2c6bc08931b4ed
unknown <ebbengabogen@gmail.com>
2019-10-14 01:56:47 +0800

    You can Now Passed array element on the third argument example: students[0].details.name, you can now sort more complex objects and arrays

--- README.md
+++ README.md
@@ -27,8 +27,7 @@ let arr = [5,2,1,7,8,4]
 ainsort.merge(arr,[1,2])
 ```
 output: [1,2,4,5,7,8]
-* the second argument should pass only two kind of array
-* first one is [1,2] second one is [2,1]
+* the second argument should pass only on this two option of array: [1,2] or [2,1]
 * if the [1,2] is passed it will be in ascending order and vice versa
 * if none is passed the default ordering is ascending
 
@@ -37,28 +36,51 @@ output: [1,2,4,5,7,8]
 let arr = [{num:2},{num:3},{num:1},{num:5},{num:4}]
 
 ainsort.merge(arr,[1,2],"num")
+
+
+//output: [{num:1},{num:2},{num:3},{num:4},{num:5}]
 ```
-output: [{num:1},{num:2},{num:3},{num:4},{num:5}]
 * the third argument should be a string
 * third argument is needed if the passed array's element is an object otherwise sorting won't work
->more Example
+## more Example
 ```javascript
 let arr = [
-  {student:{details:age:23}},
-  {student:{details:age:21}},
-  {student:{details:age:27}},
-  {student:{details:age:30}}
+  {student:{details:{age:23}},
+  {student:{details:{age:21}},
+  {student:{details:{age:27}},
+  {student:{details:{age:30}}
   ]
 
-ainsort.merge(arr,[2,1],"num.details.age")
+ainsort.merge(arr,[2,1],"student.details.age")
+
+/* output : [
+  {student:{details:{age:30}},
+  {student:{details:{age:27}},
+  {student:{details:{age:23}},
+  {student:{details:{age:21}}
+  ] */
 ```
 
-output : [
-  {student:{details:age:30}},
-  {student:{details:age:27}},
-  {student:{details:age:23}},
-  {student:{details:age:21}}
+also works with array element
+```javascript
+let arr = [
+  {student:[{details:{age:23},{name:"ain"}]},
+  {student:[{details:{age:21},{name:"grey"}]},
+  {student:[{details:{age:27},{name:"ebben"}]},
+  {student:[{details:{age:30},{name:"lolalolita"}]}
   ]
+
+ainsort.merge(arr,[2,1],"student[0].details.age")
+
+/* output : [
+  {student:[{details:{age:30},{name:"ain"}]},
+  {student:[{details:{age:27},{name:"grey"}]},
+  {student:[{details:{age:23},{name:"ebben"}]},
+  {student:[{details:{age:21},{name:"lolalolita"}]}
+  ] */
+```
+
+
 
 ---
 ## Available Sorting Algorithms
--- plugins/byArgFilter.js
+++ plugins/byArgFilter.js
@@ -1,10 +1,50 @@
 //it search the array based on given by
 const objectMiner = (obj, pass) => {
-  let ans;
+  //example: pass = "name.num[0]"
+  //it's gonna be this = ["name", "num[0]"]
   let passed = pass.split(".");
-  ans = obj[passed[0]];
+  //this whole thing will make this = ["name", "num[0]"] to this = ["name", "[num,0]"]
+  if (pass.search("]") >= 0) {
+    let newPassed = [];
+    let NewSplitted;
+    for (let j = 0; j < passed.length; j++) {
+      if (passed[j].search("]") >= 0) {
+        New = passed[j].replace(/]/g, "");
+        New = New.replace(/\[/g, " ");
+        NewSplitted = New.split(" ");
+      } else {
+        NewSplitted = passed[j];
+      }
+      newPassed.push(NewSplitted);
+    }
+    passed = newPassed;
+  }
+
+  let ans;
+  //if an element of the converted pass which is this = [name,[num,0]] is type object
+  //it will run this code which will turn this into [name,num[0]]
+  //this might be turn the converted pass into the original pass which is on line 4 of this code
+  //but this time it is not string, which is we can call the deeper elements within the given obj
+  //example: if we use num[0] from [name,num[0]] on the given obj = {num:[2,1]} it will fetch the number 2
+
+  if (typeof passed[0] === "object") {
+    ans = obj[passed[0][0]];
+    for (let k = 1; k < passed[0].length; k++) {
+      ans = ans[passed[0][k]];
+    }
+  } else {
+    ans = obj[passed[0]];
+  }
+
   for (let i = 1; i < passed.length; i++) {
-    ans = ans[passed[i]];
+    if (typeof passed[i] === "object") {
+      ans = ans[passed[i][0]];
+      for (let l = 1; l < passed[i].length; l++) {
+        ans = ans[passed[i][l]];
+      }
+    } else {
+      ans = ans[passed[i]];
+    }
   }
   return ans;
 };
@@ -14,7 +54,7 @@ const byArgFilter = (by, order, arrLeft, arrRight) => {
     //compare indexes two by two
     //swap the two indexes if the right one is lower
     //depends on the algorithm
-    //honestlt my commenting is messed up sorry
+    //honestly, my commenting is messed up sorry
     if (order === "asc") {
       return arrLeft > arrRight;
     } else {
@@ -22,6 +62,10 @@ const byArgFilter = (by, order, arrLeft, arrRight) => {
     }
   } else {
     if (order === "asc") {
+      //for filtering the given object using the passed by argument
+      //the objectMiner will search the object's(arrLeft or arrRight) element(by), given on it's first argument
+      //example: arrLeft = {name:{numbers:[12345,67890]}} by = "name.numbers[1]", it will return 67890
+      //and Compare them which is greater or lesser and then return a boolean which will be the basis of sorting depends on the sorting algorithm
       return objectMiner(arrLeft, by) > objectMiner(arrRight, by);
     } else {
       return objectMiner(arrLeft, by) < objectMiner(arrRight, by);
